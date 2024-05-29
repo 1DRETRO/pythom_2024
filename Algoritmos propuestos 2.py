ef9598c819304c8c5a9dc28abf09f2ed9d5bd816
1DRETRO <168034460+1DRETRO@users.noreply.github.com>
2024-05-29 15:07:53 -0400

    Correccion, ejercicios propuestos

--- Algoritmos propuestos 2.py
+++ Algoritmos propuestos 2.py
@@ -1,8 +1,8 @@
 Descripcion = "Este lapiz azul es de tinta"
 Descripcion = Descripcion[:50]
+descripcion_10 = Descripcion[:10]
 longitud = len(Descripcion) > 50 
 print(type(longitud))
-descripcion_10 = Descripcion[:10]
 print(f"\nDescripción: {Descripcion}")
 print(f"¿El tamaño de la cadena descripción es mayor a 50 caracteres? {longitud}")
 print(f"Los primeros 10 caracteres de la descripción: {Descripcion_10}")
--- Algoritmos propuestos 3.py
+++ Algoritmos propuestos 3.py
@@ -1,4 +1,7 @@
-Descripcion_producto = input
-Descripcion_producto = Descripcion_producto[:40]
-Longitud = len(Descripcion_producto) < 40
-input
+descripcion_producto_1=input
+descripcion_producto_2=input
+len(descripcion_producto_1) < 40
+len(descripcion_producto_2) < 40
+input(descripcion_producto_1)
+
+print()
