06c2897136d4e81a72db9079e939dc3ecdedddb5
1DRETRO <168034460+1DRETRO@users.noreply.github.com>
2024-06-14 16:42:36 -0400

    bucles, opeadores/UNIDAD 3

---
+++ Unidad 3/operadores.py
@@ -0,0 +1,20 @@
+
+niño/a <= 12
+adolescentes <= 17
+adulto <= 64
+adulto mayor <= 120   
+edad no valida >= 120
+
+edad = int(input: "Ingrese la edad de la persona: ") 
+if edad < 0:
+    categoria = "edad no valida"
+elif edad <= 12:
+    categoria = "niño/a"
+elif = edad >= 17
+    categoria = "adolescentes"  
+elif edad <= 64:
+    categoria = "adulto"
+elif edad <= 120:
+    categoria "adulto mayor"
+else:
+    categoria = "edad no valida"
