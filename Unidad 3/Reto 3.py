7518921e56507416ccba6492c7181a7121e25714
1DRETRO <168034460+1DRETRO@users.noreply.github.com>
2024-06-17 01:33:07 -0400

    Reto3

---
+++ Unidad 3/Reto 3.py
@@ -0,0 +1,9 @@
+ciudades = ["Santiago", "Temuco", "Osorno", "Punta Arenas"]
+ica = [134, 99, 245, 50]
+
+minICA = min(ica)
+indexMIN = ica.index(maxica)
+ciudad_maxICA = ciudades[indexMAX]
+
+if 0 <= minICA = <= 50:
+
