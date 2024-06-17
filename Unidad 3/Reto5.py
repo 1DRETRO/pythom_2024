a7a0c5a30053d9cd3fe1e54f17df42584554d900
1DRETRO <168034460+1DRETRO@users.noreply.github.com>
2024-06-17 02:34:31 -0400

    Terminado reto 4, 5 incompleto

--- Unidad 3/Reto5.py
+++ Unidad 3/Reto5.py
@@ -1,3 +1,7 @@
 n = int(input("Ingrese el valor de n a su preferencia:"))
-
+def n
+    if n <= 0:
+        print("La serie Fibbonaci {n} es: 0")
+    elif n = 1:
+        print("La serie de Fibbonaci {n} es: 0, 1")
 print("El resultado de las series de Fibonacci basadas en el valor asignado a su numero es", n)
