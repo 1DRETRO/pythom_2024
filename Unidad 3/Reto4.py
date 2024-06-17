3fb1c4a71e4d16aca4efa15b651315329f32cc09
1DRETRO <168034460+1DRETRO@users.noreply.github.com>
2024-06-17 02:19:51 -0400

    reto 4, 5

---
+++ Unidad 3/Reto4.py
@@ -0,0 +1,3 @@
+n = range(10, 51, 2) #numeros
+sn = sum(n) #suma de numeros
+print("La suma total de los digitos del rango 10 al 50 de 2 en 2 es", sn)
---
+++ Unidad 3/Reto5.py
@@ -0,0 +1,3 @@
+n = int(input("Ingrese el valor de n a su preferencia:"))
+
+print("El resultado de las series de Fibonacci basadas en el valor asignado a su numero es", n)
