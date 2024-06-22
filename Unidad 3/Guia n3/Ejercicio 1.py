65e3fc09c01167290dbeccf4807d5532a6cb57c1
1DRETRO <168034460+1DRETRO@users.noreply.github.com>
2024-06-21 22:31:24 -0400

    bucles y condicionales

--- Unidad 3/Guia n3/Ejercicio 1.py
+++ Unidad 3/Guia n3/Ejercicio 1.py
@@ -1,7 +1,6 @@
-
 p = "La Universidad de los Lagos es una institucion estatal fundada en 1993. Esta universidad regional entrega una contribucion significativa al desarrollo sostenible del territorio. Como Universidad sus pilares fundamentales se basan en la inclusion, pluralismo, conciencia ambiental y participacion democratica."
-minus = p.lower()
-r = minus.count("universidad")
-cp = ("universidad",) * r
+minus = p.lower() #se deja todo en minusculas para poder contar todas las palabras.
+r = minus.count("universidad") 
+cp = ("universidad",) * r 
 print(f"La palabra universidad se repite {r} veces.")
 print(f"Las palabras guardadas en la tupla:", cp)
